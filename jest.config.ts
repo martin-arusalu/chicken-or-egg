b6265b20d123812dba7b7f3dd1b2360cbdfabb93
Martin Arusalu <martin.arusalu@gmail.com>
2021-10-27 11:14:29 +0300

    Jest config to TS

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+import type {Config} from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
+};
+export default config;
